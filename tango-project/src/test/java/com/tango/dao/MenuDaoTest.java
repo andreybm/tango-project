fe13175de635390dbca21109a77f6862b97b94b4
MeckTico <enocrr@outlook.com>
2015-11-21 11:03:30 -0600

    Add Menu Dao Test

---
+++ tango-project/src/test/java/com/tango/dao/MenuDaoTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.tango.dao;
+
+/**
+ * @author Esteban Enoc
+ *
+ */
+
+import org.appfuse.dao.BaseDaoTestCase;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.tango.model.TipoComida;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class MenuDaoTest {
+
+}
